2127e7d3f9b977655a87a028b43b3d4f5b525b73
Binh.BuiDang <dangbinh4869@gmail.com>
2024-07-28 04:44:44 +0000

    add doc comment

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import type { PathParams, QueryParams, RouteDefinition } from "./types.ts";
 import { buildQueryString, interpolatePathParams } from "./utils.ts";
 
+/**
+ * Creates a route path function for a given path template.
+ *
+ * @param path - The path template containing placeholders for path parameters.
+ * @returns A function that takes path parameters and optional query parameters,
+ * and returns the interpolated path with query string if provided.
+ */
 export function createRoutePath<T extends string>(
   path: T,
 ): <Q extends QueryParams>(params: PathParams<T>, queryParams?: Q) => string {
@@ -16,6 +23,13 @@ export function createRoutePath<T extends string>(
   };
 }
 
+/**
+ * Creates a mapping of route names to their respective path generation functions.
+ *
+ * @param routes - An list of route definitions containing name and path template.
+ * @returns An object where keys are route names and values are functions that
+ * generate paths based on provided path parameters and optional query parameters.
+ */
 export function createRoutePaths<T extends RouteDefinition[]>(routes: T) {
   return routes.reduce(
     (acc, route) => {
